8ce1477acb4ab087312bcada5e2d1b2f827e45e5
Naveen <naveen.chaurasia861@gmail.com>
2023-02-26 22:45:53 +0530

    added chat gp poc

---
+++ chatGPT.py
@@ -0,0 +1,16 @@
+# # https://medium.com/geekculture/using-chatgpt-in-python-eeaed9847e72
+
+# pip install git+https://github.com/mmabrouk/chatgpt-wrapper
+# playwright install {browser}
+# chatgpt install
+
+# from chatgpt_wrapper import ChatGPT
+
+# bot = ChatGPT()
+# # return the full result
+# response = bot.ask("tell me a story about cats and dogs")
+# print(response)
+
+# # return the result in streaming (chunks)
+# for chunk in bot.ask_stream("tell me a story about cats and dogs"):
+#     print(chunk)
--- pdfToImage.py
+++ pdfToImage.py
@@ -10,3 +10,7 @@ for i in range(len(images)):
    
       # Save pages as images in the pdf
     images[i].save('page'+ str(i) +'.jpg', 'JPEG')
+
+
+
+    
