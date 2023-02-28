ad0a0e44b5ac47f927f1ba98d75cdfb8ca667f79
Naveen <naveen.chaurasia861@gmail.com>
2023-02-28 16:11:09 +0530

    added chatGPT sample code

--- chat_GPT_sample.py
+++ chat_GPT_sample.py
@@ -5,9 +5,11 @@ from chatgpt_wrapper import ChatGPT
 
 bot = ChatGPT()
 # return the full result
-response = bot.ask("tell me 5 cat names")
+bot.ask("give me only chemical reaction for the production of orthosilicic acid")
+bot.ask("convert above reaction into grams of chemicals required")
+response=bot.ask("convert above visualisation into csv file")
 print(response)
 
-# return the result in streaming (chunks)
-for chunk in bot.ask_stream("tell me a story about cats and dogs"):
-    print(chunk)
+# # return the result in streaming (chunks)
+# for chunk in bot.ask_stream("tell me a story about cats and dogs"):
+#     print(chunk)
