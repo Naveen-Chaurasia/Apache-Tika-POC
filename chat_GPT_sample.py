ed56ec6193c28185b11ee0bdf549306ef70201b9
Naveen <naveen.chaurasia861@gmail.com>
2023-03-01 15:16:40 +0530

    added chatGPT sample code

--- chat_GPT_sample.py
+++ chat_GPT_sample.py
@@ -5,11 +5,11 @@ from chatgpt_wrapper import ChatGPT
 
 bot = ChatGPT()
 # return the full result
-bot.ask("give me only chemical reaction for the production of orthosilicic acid")
-bot.ask("convert above reaction into grams of chemicals required")
-response=bot.ask("convert above visualisation into csv file")
+bot.ask("give me only chemical reaction for the production of Epoxy resin in one line")
+bot.ask("convert above reaction into grams of chemicals required to produce 1 kg of epoxy resin")
+response=bot.ask("convert above visualisation into csv file with columns: reactants and  grams required")
 print(response)
 
 # # return the result in streaming (chunks)
-# for chunk in bot.ask_stream("tell me a story about cats and dogs"):
-#     print(chunk)
+#for chunk in bot.ask_stream("convert above visualisation into csv file"): 
+#   print(chunk)
